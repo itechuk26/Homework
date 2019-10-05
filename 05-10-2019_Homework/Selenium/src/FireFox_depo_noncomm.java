451bab5a555dca89a553f32c31700def322c913d
sonal-paavan1 <sonal.paavan1@gmail.com>
2019-10-05 22:12:58 +0100

    My Homework for Selenium and Sql

---
+++ 05-10-2019_Homework/Selenium/src/FireFox_depo_noncomm.java
@@ -0,0 +1,41 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by : Divyesh Patel
+ * since : Saturday  05/10/2019
+ * Time  : 20:51
+ **/
+
+public class FireFox_depo_noncomm {
+    public static void main(String[] args)
+    {
+        String baseurl = "https://demo.nopcommerce.com/";
+        WebDriver driver;
+
+        System.setProperty("webdriver.gecko.driver","drivers/geckodriver.exe");
+        driver = new FirefoxDriver();
+
+        driver.get(baseurl);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement login_link = driver.findElement(By.linkText("Log in"));
+        login_link.click();
+
+        WebElement email_field = driver.findElement(By.id("Email"));
+        email_field.sendKeys("dev_patelin@yahoo.com");
+
+        WebElement password_field = driver.findElement(By.id("Password"));
+        password_field.sendKeys("Raja1234");
+
+        WebElement login_button = driver.findElement(By.xpath("//input[@class='button-1 login-button']"));
+        login_button.click();
+    }
+
+}
